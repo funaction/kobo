fc8cd05094537c3268057cf1d7e01105cda13882
FUNACTION <167884376+funaction@users.noreply.github.com>
2024-10-10 14:54:42 +0200

    Add files via upload

--- R/kobo.r
+++ R/kobo.r
@@ -8,7 +8,7 @@
 # author:       Daniel Romero Mujalli
 # email:        daniel.romero@supsi.ch
 #
-# last update:  20240619
+# last update:  20241010
 #
 ######################################################################
 ###############################################################
@@ -22,19 +22,27 @@
 #' PARAMETERS
 #' @param kobodata name of the funaction-kobo data file
 #'
-#' @param filename name of the output file
+#' @param outfname name of the output file
 #'
-#' @param outdir directory path where to store the file
+#' @param projectname filter by project name  (e.g., funaction)
+#'                    Use "all" to select all projects
+#'
+#' @param rm_photo_data remove data related to photos. Default TRUE
+#'
+#' @param rm_data_check_columns remove boolean data check variables.
+#'                    Default TRUE
 #'
 #' OUTPUT
 #' @return a prepared data frame (also writes data on disk to outdir)
 #'
 #' @export
-
+#'
 ######################################################################
 prepare_data <- function(kobodata
-                        ,filename = "fundata.csv"
-                        ,outdir = getwd()
+                        ,outfname = "fundata.csv"
+                        ,projectname = "funaction"
+                        ,rm_photo_data = TRUE
+                        ,rm_data_check_columns = TRUE
                         )
 {
     # check that kobodata file exists
@@ -44,72 +52,101 @@ prepare_data <- function(kobodata
     # read kobodata
     df <- read.csv(file = kobodata, header = TRUE, sep = ";")
 
-    # remove not-approved records
-    df <- df[df$X_validation_status != "Not Approved",]
+    # select data for desired project
+    if(sum(projectname != "all") > 0)
+        df <- df[df$Project %in% projectname, ]
 
-    # simplify the very long names used for leaf litter
-    # 1. find the root string
-    root <- names(df)[46]
-    # replace root by "" in selected names(df)
-    names(df) <- unlist(sapply(X = names(df)
-                              ,FUN = sub
-                              ,pattern = root
-                              ,replacement = ""
+     # Simplify country names
+    df$Country <- sub(pattern = "funaction_"
+                     ,replacement = ""
+                     ,x = df$Country
+                     )
+
+    # remove not-approved records
+    selection <- grep(pattern = "not_approved"
+                     ,x = tolower(df$X_validation_status)
+                     )
+    df <- df[-selection,]
+
+    # simplify sample_types names
+    root <- "Sample_types."
+    names(df) <- sub(pattern = root
+                     ,replacement = "sampled_"
+                     ,x = names(df)
+                     )
+
+    # simplify names used for leaf species
+    root <- "leaf_species."
+    names(df) <- sub(pattern = root
+                     ,replacement = ""
+                     ,x = names(df)
+                     )
+    
+    # simplify names for sterivex volume filtered
+    root <- "Volume_filtered_for_ivex_filter_ml_"
+    names(df) <- sub(pattern = root
+                     ,replacement = "volume"
+                     ,x = names(df)
+                     )
+    
+    # simplify names used for PA_type
+    root <- "PA_type."
+    names(df) <- sub(pattern = root
+                     ,replacement = ""
+                     ,x = names(df)
+                     )
+    
+    # simplify names for location
+    root <- "X_location_"
+    names(df) <- sub(pattern = root
+                     ,replacement = ""
+                     ,x = names(df)
+                     )
+
+    # substitute any use of character ";" by "," to avoid
+    # conflict when writing a csv file using sep = ";"
+    # 1: identify which columns make use of the char ";"
+    selection <- lapply(X = df, FUN = grep, pattern = ";")
+    selection <- unlist(lapply(X = selection
+                       ,FUN = function(x){
+                                            length(x) > 0
+                                         }
                               )
                        )
-    # add name to missing names (i.e., names(df) == ""; col 46)
-    names(df)[46] <- "leafsp"
-    
-    # simplify the name for geographic coordinates
-    geocord <- c(26:29)
-    names(df)[geocord] <- unlist(sapply(X = names(df)[geocord]
-                    ,FUN = sub, pattern = "X_Capture.your.location_"
-                    ,replacement = ""
-                    ))
-
-    # simplify names for type of protection
-    root <- paste0(names(df)[18],".")
-    names(df)[18] <- "type.protection"
-    names(df) <- unlist(sapply(X = names(df)
-                    ,FUN = sub, pattern = root, replacement = "")
-                    )
-    
-    # remove "X_", "..", "__" tags from names
-    names(df) <- sub(pattern = "X_", replacement = "", x = names(df))
-    names(df) <- gsub(pattern = "_*", replacement = "", x = names(df))
-    # remove dots repeated more than once
-    names(df) <- gsub(pattern = "[.]{2,}", replacement = "", x = names(df))
-
-    # substitute dots at first / last position by ""
-    for(name in colnames(df))
-    {
-        v <- unlist(strsplit(name, split = ""))
-        if(v[1] == ".")
-            v <- v[-1]
-        if(v[length(v)] == ".")
-            v <- v[-length(v)]
-        # concatenate character vector v and substitute the original name
-        newname <- paste(v, collapse = "")
-        colnames(df)[colnames(df) == name] <- newname
-    }
+    selection <- names(selection)[selection == TRUE]
+    df[, selection] <- gsub(pattern = ";", replacement = ","
+                           ,x = df[, selection]
+                           )
 
     # manually simplify other names
-    names(df)[6] <- "siteID"
-    names(df)[14:17] <- c("W1stvex","W2stvex","status","landuse")
-    names(df)[34:37] <- c("SPC","Cond","O2%","dO2")
+    selection <- c("EC"
+                  ,"Electric_Conductivity_C_S_cm"
+                  ,"sample_time"
+                  ,"sample_date"
+                  )
+    names(df)[names(df) %in% selection] <- c("ECspc"
+                                            ,"EC"
+                                            ,"time"
+                                            ,"date"
+                                            )
 
-    # remove boolean columns
-    root <- names(df)[7] # "Sample.types.taken"
-    selection <- names(df)[grepl(pattern = root, x = names(df))]
-    df <- df[, !names(df) %in% selection]
+    # remove photo data
+    if(rm_photo_data)
+    {
+        selection <- grep(pattern = "photo", x = tolower(names(df)))
+        df <- df[, -selection]
+    }
 
-    # remove Photo column names
-    selection <- names(df)[grepl(pattern = "Photo", x = names(df))]
-    df <- df[, !names(df) %in% selection]
+    # remove data check boolean variables
+    if(rm_data_check_columns)
+    {
+        selection <- grep(pattern = "sample|X_", x = names(df))
+        df <- df[, -selection]
+    }
 
-    # write prepared data to outdir/filename
+    # write data to outfname
     write.table(x = df
-               ,file = paste0(outdir,filename)
+               ,file = outfname
                ,quote = FALSE, row.names = FALSE
                ,sep = ";"
                )
